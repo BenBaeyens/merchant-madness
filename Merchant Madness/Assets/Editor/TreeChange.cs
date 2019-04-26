0a05e2447bf8bed06d0bbbee4644455d1ffef7b3
Kjip <kjipgamer@gmail.com>
2019-04-26 19:13:51 +0200

    Temporary color generator
    Added a temporary color generator to randomly generate branch and leaf colors of the trees, lerping between 2 colors.

---
+++ Merchant Madness/Assets/Editor/TreeChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TreeColor))]
+public class TreeChange : Editor
+{
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        TreeColor treeObject = (TreeColor)target;
+
+        if (GUILayout.Button("Change Color"))
+        {
+            treeObject.ChangeColor();
+        }
+    }
+}
---
+++ Merchant Madness/Assets/TreeColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeColor : MonoBehaviour
+{
+    [SerializeField] Material BranchColor1;
+    [SerializeField] Material BranchColor2;
+    [SerializeField] Material LeavesColor1;
+    [SerializeField] Material LeavesColor2;
+    [SerializeField] GameObject branch;
+
+    private void Start() {
+        ChangeColor();
+    }
+    public void ChangeColor() {
+        transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.Lerp(BranchColor1.color, BranchColor2.color, Random.Range(0f, 1f));
+        Transform leaveParent = gameObject.transform.Find("Leaves");
+
+        for (int i = 0; i < leaveParent.childCount; i++)
+        {
+            leaveParent.GetChild(i).gameObject.GetComponent<Renderer>().material.color = Color.Lerp(LeavesColor1.color, LeavesColor2.color, Random.Range(0f, 1f));
+        }
+    }
+}
