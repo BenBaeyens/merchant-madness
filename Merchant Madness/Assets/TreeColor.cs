e0769adac7cad26758b3cb340d7bd90b8752c4b8
Kjip <kjipgamer@gmail.com>
2019-04-26 21:05:21 +0200

    Improved tree customisation
    Imrpoved the color generator for the trees, but it is not done yet. Currently finding a way to shorten up the code by using a different method.

--- Merchant Madness/Assets/TreeColor.cs
+++ Merchant Madness/Assets/TreeColor.cs
@@ -4,17 +4,73 @@ using UnityEngine;
 
 public class TreeColor : MonoBehaviour
 {
-    [SerializeField] Material BranchColor1;
-    [SerializeField] Material BranchColor2;
-    [SerializeField] Material LeavesColor1;
-    [SerializeField] Material LeavesColor2;
-    [SerializeField] GameObject branch;
+
+    Color TreeType1Log1;
+    Color TreeType1Log2;
+    Color TreeType1Leaves1;
+    Color TreeType1Leaves2;
+
+    Color TreeType2Log1;
+    Color TreeType2Log2;
+    Color TreeType2Leaves1;
+    Color TreeType2Leaves2;
+
+    Color TreeType3Log1;
+    Color TreeType3Log2;
+    Color TreeType3Leaves1;
+    Color TreeType3Leaves2;
+
+    int treeType;
 
     private void Start() {
+        
         ChangeColor();
     }
+
+    public void ChooseTree() {
+        // 1 = birch
+        // 2 = pine
+        // 3 = pink
+
+        treeType = Random.Range(1, 3);
+    }
+
     public void ChangeColor() {
-        transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.Lerp(BranchColor1.color, BranchColor2.color, Random.Range(0f, 1f));
+
+        Material[] colors = Resources.FindObjectsOfTypeAll<Material>();
+        for (int i = 0; i < colors.Length; i++)
+        {
+            if (colors[i].name == "TreeType1Log1")
+                TreeType1Log1 = colors[i].color;
+            if (colors[i].name == "TreeType1Log2")
+                TreeType1Log2 = colors[i].color;
+            if (colors[i].name == "TreeType1Leaves1")
+                TreeType1Leaves1 = colors[i].color;
+            if (colors[i].name == "TreeType1Leaves2")
+                TreeType1Leaves2 = colors[i].color;
+
+            if (colors[i].name == "TreeType2Log1")
+                TreeType2Log1 = colors[i].color;
+            if (colors[i].name == "TreeType2Log1")
+                TreeType2Log2 = colors[i].color;
+            if (colors[i].name == "TreeType2Leaves1")
+                TreeType2Leaves1 = colors[i].color;
+            if (colors[i].name == "TreeType2Leaves2")
+                TreeType2Leaves2 = colors[i].color;
+
+            if (colors[i].name == "TreeType3Log1")
+                TreeType3Log1 = colors[i].color;
+            if (colors[i].name == "TreeType3Log1")
+                TreeType3Log2 = colors[i].color;
+            if (colors[i].name == "TreeType3Leaves1")
+                TreeType3Leaves1 = colors[i].color;
+            if (colors[i].name == "TreeType3Leaves2")
+                TreeType3Leaves2 = colors[i].color;
+        }
+
+        ChooseTree();
+
+        transform.GetChild(0).gameObject.GetComponent<Renderer>().material.color = Color.Lerp(("TreeType"+treeType+"Log1").color, BranchColor2.color, Random.Range(0f, 1f));
         Transform leaveParent = gameObject.transform.Find("Leaves");
 
         for (int i = 0; i < leaveParent.childCount; i++)
