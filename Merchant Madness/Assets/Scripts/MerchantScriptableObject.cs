662d99417a5eab9eab768c9095fddb2ffd48d551
Kjip <kjipgamer@gmail.com>
2019-05-04 12:58:35 +0200

    Cleaned up scriptable merchant object

--- Merchant Madness/Assets/Scripts/MerchantScriptableObject.cs
+++ Merchant Madness/Assets/Scripts/MerchantScriptableObject.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Merchant", menuName = "Merchant")]
 public class MerchantScriptableObject : ScriptableObject
 {
+    [Header("Merchant Info")]
     public new string name;
     public string welcomeText;
     public string thankYouText;
 
+    [Header("1 = GREEN, 2 = RED, 3 = BLUE, 4 = YELLOW")]
+    public int color;
+
+    [Header("Total amount of items this merchant sells")]
     public int totalItemAmount;
 
+    [Header("Item Info")]
     public string itemDisplayName;
     public string itemCodeName;
     public int itemAmount;
