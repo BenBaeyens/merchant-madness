afda29ff6c50ce9c825f6d655945fc79bf5bf033
Kjip <kjipgamer@gmail.com>
2019-05-05 14:49:44 +0200

    Tree chopping functionality

---
+++ Merchant Madness/Assets/TreeHitboxScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeHitboxScript : MonoBehaviour
+{
+    public bool isPlayerInRange = false;
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.name == "Player")
+            isPlayerInRange = true;
+    }
+    private void OnTriggerExit(Collider other) {
+        if(other.name == "Player")
+            isPlayerInRange = false;
+    }
+}
---
+++ Merchant Madness/Assets/TreeScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour {
+
+    public float treeHealth;
+
+    bool hasAxe = false;
+
+    public GameObject treeRange;
+    public GameObject player;
+    public Player pl;
+
+    private void Start() {
+        player = GameObject.Find("Player");
+        pl = player.GetComponent<Player>();
+    }
+
+    private void Update() {
+
+
+        if (!hasAxe) {
+            for (int i = 0; i < pl.inventory.Count; i++)
+            {
+                if (pl.inventory[i].Contains("axe"))
+                {
+                    hasAxe = true;
+                    break;
+                }
+            }
+        }
+
+        if (treeRange.GetComponent<TreeHitboxScript>().isPlayerInRange
+            && hasAxe
+            && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("hit");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitobj = hit.transform.gameObject;
+                if (hitobj.name == "TreeActivationRange" && hitobj.transform.parent.name == gameObject.name)
+                {
+                    treeHealth = TreeDamage();
+                }
+            }
+
+           
+                Debug.Log(treeHealth);
+                Debug.Log(TreeDamage());
+            
+        }
+
+
+        if(treeHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    float TreeDamage() {
+        if (pl.inventory.Contains("axe_wooden"))
+            return treeHealth - 1f;
+        else
+            return 0f;
+    }
+
+    
+
+
+
+
+
+
+
+}
