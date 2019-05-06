f4e48a7053dc37cec6098aea9c924e2c707b1d51
Kjip <kjipgamer@gmail.com>
2019-05-06 21:04:39 +0200

    Added item scriptable object

---
+++ Merchant Madness/Assets/Scripts/ItemScriptableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+public class ItemScriptableObject : ScriptableObject {
+    [Header("Item Info")]
+    public string itemDisplayName;
+    public string itemCodeName;
+    public string itemLore;
+
+    [Header("Item Type")]
+    public itemType itemtype;
+
+    [Header("Item Stats")]
+    public float entityDefaultDamage;
+    public float treeDefaultDamage;
+    public float rockDefaultDamage;
+
+
+    public enum itemType {Item, Sword, Axe, Pickaxe, Special }
+}
