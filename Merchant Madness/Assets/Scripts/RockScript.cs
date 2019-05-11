e381c566aa4f30f199b18d8e17da23924ed5bcb3
Kjip <kjipgamer@gmail.com>
2019-05-11 14:23:02 +0200

    Added rock prefab

---
+++ Merchant Madness/Assets/Scripts/RockScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockScript : MonoBehaviour {
+
+    public float treeHealth;
+
+    bool hasPickaxe = false;
+
+    public GameObject treeRange;
+    public GameObject player;
+    public Player pl;
+
+    private void Start() {
+        player = GameObject.Find("Player");
+        pl = player.GetComponent<Player>();
+    }
+
+    private void Update() {
+
+
+        if (!hasPickaxe) {
+            for (int i = 0; i < pl.inventory.Count; i++)
+            {
+                if (pl.inventory[i].Contains("axe"))
+                {
+                    hasPickaxe = true;
+                    break;
+                }
+            }
+        }
+
+        if (treeRange.GetComponent<TreeHitboxScript>().isPlayerInRange
+            && hasPickaxe
+            && Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("hit");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject hitobj = hit.transform.gameObject;
+                if (hitobj.name == "TreeActivationRange" && hitobj.transform.parent.name == gameObject.name)
+                {
+                    treeHealth = TreeDamage();
+                    pl.inventory.Add("wooden_log");
+                }
+            }
+        }
+
+
+        if(treeHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    float TreeDamage() {
+        if (pl.inventory.Contains("axe_wooden"))
+            return treeHealth - 1f;
+        else
+            return 0f;
+    }
+
+    
+
+
+
+
+
+
+
+}
